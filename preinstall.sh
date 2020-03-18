945803641ceacb14ca8636c077ade7a045a73419
Daniel <44267666+ghostinthecable@users.noreply.github.com>
2020-03-18 01:01:33 -0700

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -30,6 +30,64 @@ fi
 
 mv SECSUITE/* /root/scripts/SECSUITE/
 mv README.md /root/scripts/SECSUITE/
+
+if [ -d "/root/scripts/SECSUITE/inframon/apachestatus/" ]; then
+        printf " -> /root/scripts/SECSUITE/inframon/apachestatus/ ${green}✓${nc}\n"
+    else
+        printf " -> /root/scripts/SECSUITE/inframon/apachestatus/ ${red}X${nc}\n"
+        printf "+ Creating /root/scripts/SECSUITE/inframon/apachestatus/ (base directory)\n"
+        mkdir /root/scripts/SECSUITE/inframon/apachestatus/
+        if [ -d "/root/scripts/SECSUITE/inframon/apachestatus/" ]; then
+                printf " -> /root/scripts/SECSUITE/inframon/apachestatus/ ${green}✓${nc}\n"
+        fi
+fi
+
+if [ -d "/root/scripts/SECSUITE/inframon/tempfiles/" ]; then
+        printf " -> /root/scripts/SECSUITE/inframon/tempfiles/ ${green}✓${nc}\n"
+    else
+        printf " -> /root/scripts/SECSUITE/inframon/tempfiles/ ${red}X${nc}\n"
+        printf "+ Creating /root/scripts/SECSUITE/inframon/tempfiles/ (base directory)\n"
+        mkdir /root/scripts/SECSUITE/inframon/tempfiles/
+        if [ -d "/root/scripts/SECSUITE/inframon/tempfiles/" ]; then
+                printf " -> /root/scripts/SECSUITE/inframon/tempfiles/ ${green}✓${nc}\n"
+        fi
+fi
+
+if [ -d "/root/scripts/SECSUITE/inframon/temperaturefiles/" ]; then
+        printf " -> /root/scripts/SECSUITE/inframon/temperaturefiles/ ${green}✓${nc}\n"
+    else
+        printf " -> /root/scripts/SECSUITE/inframon/temperaturefiles/ ${red}X${nc}\n"
+        printf "+ Creating /root/scripts/SECSUITE/inframon/temperaturefiles/ (base directory)\n"
+        mkdir /root/scripts/SECSUITE/inframon/temperaturefiles/
+        if [ -d "/root/scripts/SECSUITE/inframon/temperaturefiles/" ]; then
+                printf " -> /root/scripts/SECSUITE/inframon/temperaturefiles/ ${green}✓${nc}\n"
+        fi
+fi
+
+if [ -d "/root/scripts/SECSUITE/inframon/latency-files/" ]; then
+        printf " -> /root/scripts/SECSUITE/inframon/latency-files/ ${green}✓${nc}\n"
+    else
+        printf " -> /root/scripts/SECSUITE/inframon/latency-files/ ${red}X${nc}\n"
+        printf "+ Creating /root/scripts/SECSUITE/inframon/latency-files/ (base directory)\n"
+        mkdir /root/scripts/SECSUITE/inframon/latency-files/
+        if [ -d "/root/scripts/SECSUITE/inframon/latency-files/" ]; then
+                printf " -> /root/scripts/SECSUITE/inframon/latency-files/ ${green}✓${nc}\n"
+        fi
+fi
+
+
+if [ -d "/root/scripts/SECSUITE/inframon/cpufiles/" ]; then
+        printf " -> /root/scripts/SECSUITE/inframon/cpufiles/ ${green}✓${nc}\n"
+    else
+        printf " -> /root/scripts/SECSUITE/inframon/cpufiles/ ${red}X${nc}\n"
+        printf "+ Creating /root/scripts/SECSUITE/inframon/cpufiles/inframon/cpufiles/ (base directory)\n"
+        mkdir /root/scripts/SECSUITE/inframon/cpufiles/
+        if [ -d "/root/scripts/SECSUITE/inframon/cpufiles/" ]; then
+                printf " -> /root/scripts/SECSUITE/inframon/cpufiles/ ${green}✓${nc}\n"
+        fi
+fi
+
 rm -rf SECSUITE/
 rm -rf ../secsuite-production/
+
 exit
