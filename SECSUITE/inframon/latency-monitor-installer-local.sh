3a350eae636834829b7588c38c53a3fe030144e1
Daniel <44267666+ghostinthecable@users.noreply.github.com>
2020-03-27 17:40:09 +0100

    Update latency-monitor-installer-local.sh

--- SECSUITE/inframon/latency-monitor-installer-local.sh
+++ SECSUITE/inframon/latency-monitor-installer-local.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 #
-#This script is intended for SECSUITE users to configure the Network Monitor component for the System Dashboard.
-#Written by Daniel Ward
-#Version 1.250120
 #Global Variables
-#(You may add your MySQL Creds in here if you prefer)
-mysqluser="USERNAME"
-mysqlpass="PASSWORD"
-#ASCII Colours, for user-friendliness
+#MySQL Creds
+mysqluser="user"
+mysqlpass="pass"
+#ASCII Colours
 red='\033[0;31m'
 green='\033[0;32m'
 nc='\033[0m'
+#
+basedir="/root/scripts/SECSUITE/inframon"
+dbf="$basedir/dbfile.txt"
+dbf1="$basedir/dbfile1.txt"
+dbid="$basedir/dbidnums.txt"
+lid="$basedir/lastid.txt"
+sclid="$basedir/sclastid.txt"
+qry1="$basedir/qry1.txt"
+qry2="$basedir/qry2.txt"
+#
 #Begin
 #
 printf "${green} ____  _____ ____ ____  _   _ ___ _____ _____${nc}\n"
@@ -20,6 +27,7 @@ printf "${green} ___) | |__| |___ ___) | |_| || |  | | | |___${nc}\n"
 printf "${green}|____/|_____\____|____/ \___/|___| |_| |_____|${nc}\n"
 printf "${green}           LATENCY-MONITOR-INSTALLER${nc}\n"
 #
+echo ''
 while true; do
     read -p "Have you already configured your Network Monitor Database Credentials? (y/n): " yn
     case $yn in
@@ -52,54 +60,50 @@ while true; do
     esac
 done
 echo ''
-mysqlshow --user=$mysqluser --password=$mysqlpass status >> dbfile.txt 2>/dev/null
-if grep -q status "dbfile.txt"; then
+mysqlshow --user=$mysqluser --password=$mysqlpass status >> $dbf 2>/dev/null
+if grep -q "status" "$dbf"; then
         echo "Database is Existing."
+    else
+        mysql --user="$mysqluser" --password="$mysqlpass" -e "CREATE DATABASE IF NOT EXISTS status;"
 fi
-rm dbfile.txt
-#*******************************
-#CHECK if Monitor script exists:
-#*******************************
-#config="config.txt"
-#monitor="/root/scripts/SECSUITE/inframon/monitor.sh"
-#if [ -f "$monitor" ]; then
+rm $dbf
 #****************************
 #START Database Configuration
 #****************************
 echo "Checking Database Configuration..."
-mysqlshow --user=$mysqluser --password=$mysqlpass status >> dbfile.txt 2>/dev/null
-if grep -q "srv" "dbfile.txt"; then
-        printf "${green} Table 'status.srv' exists, continuing...${nc}\n"
+mysqlshow --user=$mysqluser --password=$mysqlpass status >> $dbf 2>/dev/null
+if grep -q "srv" "$dbf"; then
+        printf "${green}Table 'status.srv' exists, continuing...${nc}\n"
 fi
 if ! grep -q "srv" "dbfile.txt"; then
-        printf "${red} Table 'status.srv' Doesn't exist, Installing...${nc}\n"
+        printf "${red}Table 'status.srv' Doesn't exist, Installing...${nc}\n"
         mysql --user=$mysqluser --password=$mysqlpass -e "USE status;CREATE TABLE srv (id INT AUTO_INCREMENT NOT NULL, hostname VARCHAR(255) NOT NULL, lastping VARCHAR(255) NOT NULL, PRIMARY KEY (id));" 2>/dev/null
-    mysqlshow --user=$mysqluser --password=$mysqlpass status >> dbfile2.txt 2>/dev/null
-	if grep -q "srv" "dbfile2.txt"; then
+        mysqlshow --user=$mysqluser --password=$mysqlpass status >> $dbf1 2>/dev/null
+	if grep -q "srv" "$dbf1"; then
 	printf "${green}Table 'status.srv' has been created, continuing...${nc}\n"
 	fi
 fi
-rm dbfile.txt dbfile2.txt
-
+rm $dbf $dbf1
+#
 echo "Checking Historical Database Configuration..."
-mysqlshow --user=$mysqluser --password=$mysqlpass status >> dbfile.txt 2>/dev/null
-if grep -q "hist_srv" "dbfile.txt"; then
-        printf "${green} Table 'status.hist_srv' exists, continuing...${nc}\n"
+mysqlshow --user=$mysqluser --password=$mysqlpass status >> $dbf 2>/dev/null
+if grep -q "hist_srv" "$dbf"; then
+        printf "${green}Table 'status.hist_srv' exists, continuing...${nc}\n"
 fi
-if ! grep -q "hist_srv" "dbfile.txt"; then
-        printf "${red} Table 'status.hist_srv' Doesn't exist, Installing...${nc}\n"
+if ! grep -q "hist_srv" "$dbf"; then
+        printf "${red}Table 'status.hist_srv' Doesn't exist, Installing...${nc}\n"
         mysql --user=$mysqluser --password=$mysqlpass -e "USE status;CREATE TABLE hist_srv (id INT NOT NULL, hostname VARCHAR(255) NOT NULL, lastping VARCHAR(255) NOT NULL, importtime VARCHAR(255) NOT NULL, random VARCHAR(255) NOT NULL, PRIMARY KEY (random));" 2>/dev/null
-    mysqlshow --user=$mysqluser --password=$mysqlpass status >> dbfile2.txt 2>/dev/null
-	if grep -q "hist_srv" "dbfile2.txt"; then
+    mysqlshow --user=$mysqluser --password=$mysqlpass status >> $dbf1 2>/dev/null
+	if grep -q "hist_srv" "$dbf1"; then
 	printf "${green}Table 'status.hist_srv' has been created, continuing...${nc}\n"
 	fi
 fi
-rm dbfile.txt dbfile2.txt
-
+rm $dbf $dbf1
+#
 echo "Configuring Database for new entry..."
 #Get the ID numbers from the current database, add one, and use that as the ID for the new one.
-mysql --user=$mysqluser --password=$mysqlpass -e "use status;SELECT ID FROM srv;" > dbidnums.txt 2>/dev/null
-i=$(awk '/./{line=$0} END{print line}' dbidnums.txt)
+mysql --user=$mysqluser --password=$mysqlpass -e "use status;SELECT ID FROM srv;" > $dbid 2>/dev/null
+i=$(awk '/./{line=$0} END{print line}' $dbid)
 #Add one to make it acceptable
 ((i++))
 #Import Web Server
@@ -115,7 +119,7 @@ mysql --user=$mysqluser --password=$mysqlpass -e "USE status; INSERT INTO srv (i
 echo "SSH Server Inserted with ID Number $i: "
 mysql --user=$mysqluser --password=$mysqlpass -e "USE status; SELECT * FROM srv WHERE id = '$i';" 2>/dev/null
 #Remove source
-rm dbidnums.txt
+rm $dbid
 #**************************
 #END Database Configuration
 #**************************
@@ -124,16 +128,15 @@ rm dbidnums.txt
 #**************************
 echo "Configuring Script for new entry..."
 #Get Current Amount of Configured Assets
-mysql --user=$mysqluser --password=$mysqlpass -e "use status;SELECT ID FROM srv;" > dbidnums.txt 2>/dev/null
-i=$(awk '/./{line=$0} END{print line}' dbidnums.txt)
+mysql --user=$mysqluser --password=$mysqlpass -e "use status;SELECT ID FROM srv;" > $dbid 2>/dev/null
+i=$(awk '/./{line=$0} END{print line}' $dbid)
 #legacy:
-#cat /root/scripts/SECSUITE/inframon/net-monitor.sh | grep -E ".csv" > scidnums.txt
 #Get All ID Numbers (smallest first, largest last)
-cat dbidnums.txt | grep -oP '^[^0-9]*\K[0-9]+' > lastid.txt
+cat $dbid | grep -oP '^[^0-9]*\K[0-9]+' > $lid
 #echo "ID Numbers to be configured: "
 #Select last line of file
-cat lastid.txt | awk '/./{line=$0} END{print line}' > sclastid.txt
-i=$(awk '/./{line=$0} END{print line}' sclastid.txt)
+cat $lid | awk '/./{line=$0} END{print line}' > $sclid
+i=$(awk '/./{line=$0} END{print line}' $sclid)
 two="2"
 number=$(echo $[$i - $two])
 #Add one to make it acceptable
@@ -143,7 +146,7 @@ number=$(echo $[$i - $two])
 ((number++))
 #echo "SSH Server ID: " $number
 #Remove source
-rm lastid.txt sclastid.txt dbidnums.txt
+rm $lid $sclid $dbid
 #Perform maths, for reasons
 #Web Maths:
 two="2"
@@ -153,7 +156,7 @@ filenumname="file$filenum='"
 #echo "Web Server ID to be inserted:"
 #echo "file$filenum"
 #Build file name
-webfilename=$(echo "file$filenum='/root/scripts/SECSUITE/inframon/tempfiles/file$filenum.csv'")
+webfilename=$(echo "file$filenum='$basedir/tempfiles/file$filenum.csv'")
 #echo "Web File Name:"
 #echo $webfilename
 #sed -i "/$filenumname/a $webfilename" /root/scripts/SECSUITE/inframon/net-monitor.sh
@@ -167,7 +170,7 @@ filenumnamessh="file$filenumssh='"
 #echo "SSH Server ID to be inserted:"
 #echo "file$filenumssh"
 #Build file name
-sshfilename=$(echo "file$filenumssh='/root/scripts/SECSUITE/inframon/tempfiles/file$filenumssh.csv'")
+sshfilename=$(echo "file$filenumssh='$basedir/tempfiles/file$filenumssh.csv'")
 #echo "SSH File Name:"
 #echo $sshfilename
 #sed -i "/$filenumnamessh/a $sshfilename" /root/scripts/SECSUITE/inframon/net-monitor.sh
@@ -187,7 +190,7 @@ mysql --user=$mysqluser --password=$mysqlpass -e "USE status; INSERT INTO srv (i
 #START Script Creation
 #**************************
 #Define Variables for passing;
-buildfile="/root/scripts/SECSUITE/inframon/latency-files/$hostname-latency-monitor.sh"
+buildfile="$basedir/latency-files/$hostname-latency-monitor.sh"
 echo "#!/bin/bash" >> $buildfile
 echo "#" >> $buildfile
 echo "#Global Variables" >> $buildfile
@@ -198,8 +201,8 @@ echo "red='\033[0;31m'" >> $buildfile
 echo "green='\033[0;32m'" >> $buildfile
 echo "nc='\033[0m'" >> $buildfile
 echo "#" >> $buildfile
-echo "file01='/root/scripts/SECSUITE/inframon/tempfiles/01.csv'" >> $buildfile
-echo "file02='/root/scripts/SECSUITE/inframon/tempfiles/02.csv'" >> $buildfile
+echo "file01='$basedir/tempfiles/01.csv'" >> $buildfile
+echo "file02='$basedir/tempfiles/02.csv'" >> $buildfile
 echo "#" >> $buildfile
 echo "# << BEGIN HOSTS >>" >> $buildfile
 echo "#" >> $buildfile
@@ -218,23 +221,23 @@ echo "if [ $ SUCCESS -eq 0 ]" >> $buildfile
 echo "then" >> $buildfile
 echo "  nmap $ hostdev -p$wsp > $ file01" >> $buildfile
 echo "  lastping01=$ (cat $ file01 | grep 'latency')" >> $buildfile
-echo "mysql --user=$ user --password=$ pass -e " >> qry1.txt
-echo '"' >> qry1.txt
-echo "use status;update srv set lastping = '$ lastping01' where id='$filenum';" >> qry1.txt
-echo '"' >> qry1.txt
-echo " 2>/dev/null ; " >> qry1.txt
-tr -d '\n' < qry1.txt >> $buildfile
-rm qry1.txt
+echo "mysql --user=$ user --password=$ pass -e " >> $qry1
+echo '"' >> $qry1
+echo "use status;update srv set lastping = '$ lastping01' where id='$filenum';" >> $qry1
+echo '"' >> $qry1
+echo " 2>/dev/null ; " >> $qry1
+tr -d '\n' < $qry1 >> $buildfile
+rm $qry1
 echo "else" >> $buildfile
 echo "  nmap $ hostdev -p$wsp > $ file01" >> $buildfile
 echo "  lastping01=$ (cat $ file01 | grep 'latency')" >> $buildfile
-echo "  mysql --user=$ user --password=$ pass -e " >> qry2.txt
-echo '"' >> qry2.txt
-echo "use status;update srv set lastping = '$ lastping01' where id='$filenum';" >> qry2.txt
-echo '"' >> qry2.txt
-echo " 2>/dev/null ; " >> qry2.txt
-tr -d '\n' < qry2.txt >> $buildfile
-rm qry2.txt
+echo "  mysql --user=$ user --password=$ pass -e " >> $qry2
+echo '"' >> $qry2
+echo "use status;update srv set lastping = '$ lastping01' where id='$filenum';" >> $qry2
+echo '"' >> $qry2
+echo " 2>/dev/null ; " >> $qry2
+tr -d '\n' < $qry2 >> $buildfile
+rm $qry2
 echo "fi" >> $buildfile
 echo "#EOF" >> $buildfile
 echo "#'++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'" >> $buildfile
@@ -264,23 +267,23 @@ echo "if [ $ SUCCESS -eq 0 ]" >> $buildfile
 echo "then" >> $buildfile
 echo "  nmap $ hostdev -p$sshp > $ file02" >> $buildfile
 echo "  lastping02=$ (cat $ file02 | grep 'latency')" >> $buildfile
-echo "mysql --user=$ user --password=$ pass -e " >> qry1.txt
-echo '"' >> qry1.txt
-echo "use status;update srv set lastping = '$ lastping02' where id='$filenumssh';" >> qry1.txt
-echo '"' >> qry1.txt
-echo " 2>/dev/null ; " >> qry1.txt
-tr -d '\n' < qry1.txt >> $buildfile
-rm qry1.txt
+echo "mysql --user=$ user --password=$ pass -e " >> $qry1
+echo '"' >> $qry1
+echo "use status;update srv set lastping = '$ lastping02' where id='$filenumssh';" >> $qry1
+echo '"' >> $qry1
+echo " 2>/dev/null ; " >> $qry1
+tr -d '\n' < $qry1 >> $buildfile
+rm $qry1
 echo "else" >> $buildfile
 echo "  nmap $ hostdev -p$sshp > $ file02" >> $buildfile
 echo "  lastping02=$ (cat $ file02 | grep 'latency')" >> $buildfile
-echo "  mysql --user=$ user --password=$ pass -e " >> qry2.txt
-echo '"' >> qry2.txt
-echo "use status;update srv set lastping = '$ lastping02' where id='$filenumssh';" >> qry2.txt
-echo '"' >> qry2.txt
-echo " 2>/dev/null ; " >> qry2.txt
-tr -d '\n' < qry2.txt >> $buildfile
-rm qry2.txt
+echo "  mysql --user=$ user --password=$ pass -e " >> $qry2
+echo '"' >> $qry2
+echo "use status;update srv set lastping = '$ lastping02' where id='$filenumssh';" >> $qry2
+echo '"' >> $qry2
+echo " 2>/dev/null ; " >> $qry2
+tr -d '\n' < $qry2 >> $buildfile
+rm $qry2
 echo "fi" >> $buildfile
 echo 'mysql --user=$ user --password=$ pass -e "USE status;INSERT INTO hist_srv (id, hostname, lastping, random, importtime) SELECT *, RAND(), CURRENT_TIMESTAMP() FROM srv;" 2>/dev/null' >> $buildfile
 echo "rm $ file01 $ file02" >> $buildfile
@@ -305,10 +308,10 @@ echo "Your current hosts: "
 mysql --user=$mysqluser --password=$mysqlpass -e "USE status; SELECT * FROM srv;" 2>/dev/null
 echo ''
 echo "Your network monitor script for $hostname: "
-FILE=/root/scripts/SECSUITE/inframon/latency-files/$hostname-latency-monitor.sh
+FILE=$basedir/latency-files/$hostname-latency-monitor.sh
 if test -f "$FILE"; then
     printf "+ ${green} $FILE ${nc} has been created successfully. Executing...\n"
-    bash /root/scripts/SECSUITE/inframon/latency-files/$hostname-latency-monitor.sh
+    bash $basedir/latency-files/$hostname-latency-monitor.sh
     mysql --user="$mysqluser" --password="$mysqlpass" -e "use status; select * from srv;" 2>/dev/null
     mysql --user="$mysqluser" --password="$mysqlpass" -e "use status; select * from hist_srv;" 2>/dev/null
 fi
